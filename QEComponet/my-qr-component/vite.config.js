b24b2e92707f1865f70868ca2d5ec03950618d8f
AlexGuar <jonnathanguartanjg@gmail.com>
2026-08-30 20:43:12 -0500

    commit 1

--- QEComponet/my-qr-component/vite.config.js
+++ QEComponet/my-qr-component/vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // Configura la ruta base pública donde se alojará el proyecto en GitHub Pages.
   // Debe empezar y terminar obligatoriamente con una barra '/'.
-  base: '/frontend-mentor-challenges/',
+  base: '/frontend-mentor-challenges/QEComponet/my-qr-component/',
 })
